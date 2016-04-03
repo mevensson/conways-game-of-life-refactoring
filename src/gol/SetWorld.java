90d1fa8343b77179cf6f0873c9557c4f884e7fd8
Mattias Evensson <mattias@evensson.eu>
2016-04-03 08:55:14 +0200

    Remove unused methods from World

--- src/gol/SetWorld.java
+++ src/gol/SetWorld.java
@@ -7,11 +7,6 @@ import java.util.Set;
 public class SetWorld implements World {
 	private final Set<Point> world;
 
-	private int minX = Integer.MAX_VALUE;
-	private int maxX = Integer.MIN_VALUE;
-	private int minY = Integer.MAX_VALUE;
-	private int maxY = Integer.MIN_VALUE;
-
 	public SetWorld() {
 		world = new HashSet<>();
 	}
@@ -21,53 +16,12 @@ public class SetWorld implements World {
 	}
 
 	@Override
-	public int heightOffset() {
-		if (world.isEmpty()) {
-			return 0;
-		}
-
-		return minY;
-	}
-
-	@Override
-	public int widthOffset() {
-		if (world.isEmpty()) {
-			return 0;
-		}
-
-		return minX;
-	}
-
-	@Override
-	public int height() {
-		if (world.isEmpty()) {
-			return 0;
-		}
-
-		return maxY - minY + 1;
-	}
-
-	@Override
-	public int width() {
-		if (world.isEmpty()) {
-			return 0;
-		}
-
-		return maxX - minX + 1;
-	}
-
-	@Override
 	public boolean isAlive(int x, int y) {
 		return world.contains(new Point(x, y));
 	}
 
 	@Override
 	public void setAlive(int x, int y) {
-		minX = Math.min(minX, x);
-		maxX = Math.max(maxX, x);
-		minY = Math.min(minY, y);
-		maxY = Math.max(maxY, y);
-
 		world.add(new Point(x, y));
 	}
 
--- src/gol/World.java
+++ src/gol/World.java
@@ -2,11 +2,6 @@ package gol;
 
 public interface World extends Iterable<Point> {
 
-	int heightOffset();
-	int widthOffset();
-	int height();
-	int width();
-
 	boolean isAlive(int x, int y);
 	void setAlive(int x, int y);
 	int numAlive();
