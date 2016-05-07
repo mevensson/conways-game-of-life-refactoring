9abbfaccf31251a07d7a56bf721d66867ae7f311
Mattias Evensson <mattias@evensson.eu>
2016-05-07 11:46:56 +0200

    Reformat Arguments class

--- src/gol/Arguments.java
+++ src/gol/Arguments.java
@@ -11,47 +11,64 @@ public class Arguments {
 	private static final int DEFAULT_HEIGHT = 15;
 	private static final int DEFAULT_WIDTH = 20;
 	private static final int DEFAULT_STEPS = 100;
-	private static final int DEFAULT_STEP_DELAY = -1;
 	private static final int DEFAULT_HISTORY_LENGTH = 0;
+	private static final int DEFAULT_STEP_DELAY = -1;
 
-	private final StringArgument filename = new StringArgument(
-			"-f",
-			"FILE_PATH",
-			"File with start state. Default is a random start state.");
-	private final IntegerArgument height = new IntegerArgument(
-			"-h",
-			"HEIGHT",
-			"Height of simulation view port. Default is 15.",
-			DEFAULT_HEIGHT);
-	private final IntegerArgument width = new IntegerArgument(
-			"-w",
-			"WIDTH",
-			"Width of simulation view port. Default is 20.",
-			DEFAULT_WIDTH);
-	private final IntegerArgument steps = new IntegerArgument(
-			"-s",
-			"STEPS",
-			"Number of maximum generation steps. Default is 100.",
-			DEFAULT_STEPS);
-	private final IntegerArgument stepDelay = new IntegerArgument(
-			"-t",
-			"MS",
-			"Time delay (ms) to wait between each step. Default is 0 ms.",
-			DEFAULT_STEP_DELAY);
-	private final IntegerArgument historyLength = new IntegerArgument(
-			"-l",
-			"X",
-			"Detect loops of maximum length x. Default is 0 - no loop detection.",
-			DEFAULT_HISTORY_LENGTH);
-	private final BoolArgument atFormat = new BoolArgument(
-			"-@",
-			"Use spaced '@' and '.' instead of default '#' and '-'.");
-	private final BoolArgument oFormat = new BoolArgument(
-			"-O",
-			"Use 'O' instead of default '#'.");
-	private final BoolArgument quietMode = new BoolArgument(
-			"-q",
-			"Quiet mode. Only outputs the last step in a simulation. Ignores time delay.");
+	private final IntegerArgument width =
+			new IntegerArgument(
+					"-w",
+					"WIDTH",
+					"Width of simulation view port. Default is 20.",
+					DEFAULT_WIDTH);
+
+	private final IntegerArgument height =
+			new IntegerArgument(
+					"-h",
+					"HEIGHT",
+					"Height of simulation view port. Default is 15.",
+					DEFAULT_HEIGHT);
+
+	private final StringArgument filename =
+			new StringArgument(
+					"-f",
+					"FILE_PATH",
+					"File with start state. Default is a random start state.");
+
+	private final BoolArgument atFormat =
+			new BoolArgument(
+					"-@",
+					"Use spaced '@' and '.' instead of default '#' and '-'.");
+
+	private final BoolArgument oFormat =
+			new BoolArgument(
+					"-O",
+					"Use 'O' instead of default '#'.");
+
+	private final IntegerArgument steps =
+			new IntegerArgument(
+					"-s",
+					"STEPS",
+					"Number of maximum generation steps. Default is 100.",
+					DEFAULT_STEPS);
+
+	private final IntegerArgument historyLength =
+			new IntegerArgument(
+					"-l",
+					"X",
+					"Detect loops of maximum length x. Default is 0 - no loop detection.",
+					DEFAULT_HISTORY_LENGTH);
+
+	private final IntegerArgument stepDelay =
+			new IntegerArgument(
+					"-t",
+					"MS",
+					"Time delay (ms) to wait between each step. Default is 0 ms.",
+					DEFAULT_STEP_DELAY);
+
+	private final BoolArgument quietMode =
+			new BoolArgument(
+					"-q",
+					"Quiet mode. Only outputs the last step in a simulation. Ignores time delay.");
 
 	public Optional<String> getFilename() {
 		return filename.getArgument();
